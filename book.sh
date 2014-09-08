c36d58eac2ff2c62c3d1874d2685c3b715a750ef
ivanbrennan <ivan.brennan@gmail.com>
2014-09-07 20:10:34 -0400

    more tmux

---
+++ book.sh
@@ -0,0 +1,13 @@
+if ! tmux has-session -t book 2>/dev/null
+then
+  tmux new-session -d -s book   -n shell   -c ~/Development/code/handybook
+
+  tmux new-window     -t book:5 -n server  -c ~/Development/code/handybook
+  tmux new-window     -t book:6 -n git     -c ~/Development/code/handybook
+  tmux new-window     -t book:7 -n console -c ~/Development/code/handybook
+  tmux new-window     -t book:8 -n tests   -c ~/Development/code/handybook
+  tmux new-window     -t book:9 -n vim     -c ~/Development/code/handybook
+
+  tmux select-window  -t book:0
+fi
+tmux attach-session -t book
