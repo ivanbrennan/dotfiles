a767c79549465027aad005731342a348b00b01df
ivanbrennan <ivan.brennan@gmail.com>
2014-10-06 21:40:46 -0400

    tweak tmux for work

--- hb-dev.sh
+++ hb-dev.sh
@@ -1,8 +1,8 @@
 if ! tmux has-session -t hb-dev 2>/dev/null
 then
-  tmux new-session -d -s hb-dev   -n vim     -c ~/Development/code/handybook
+  tmux new-session -d -s hb-dev   -n shell   -c ~/Development/code/handybook
   tmux new-window     -t hb-dev:8 -n console -c ~/Development/code/handybook
-  tmux new-window     -t hb-dev:9 -n shell   -c ~/Development/code/handybook
+  tmux new-window     -t hb-dev:9 -n vim     -c ~/Development/code/handybook
 
   tmux select-window  -t hb-dev:0
 fi
--- hb-server.sh
+++ hb-server.sh
@@ -1,7 +1,9 @@
 if ! tmux has-session -t hb-server 2>/dev/null
 then
   tmux new-session -d -s hb-server   -n shell   -c ~/Development/code/handybook
-  tmux new-window     -t hb-server:9 -n server  -c ~/Development/code/handybook
+  tmux new-window     -t hb-server:1 -n view    -c ~/Development/code/handybook
+  tmux new-window     -t hb-server:8 -n server  -c ~/Development/code/handybook
+  tmux new-window     -t hb-server:9 -n ssh     -c ~/Development/code/handybook
 
   tmux select-window  -t hb-server:0
 fi
