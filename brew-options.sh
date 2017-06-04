49969026fb6916119ad2d506257d793af8323138
ivanbrennan <ivan.brennan@gmail.com>
2017-06-04 18:45:50 -0400

    brew-options.sh
    Script to check which packages were brewed with options, and what the option
    flags were.

---
+++ brew-options.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Script to check which packages were brewed with options, and what the option
+# flags were.
+
+brew info --json=v1 --installed \
+  | jq 'map({name: .name, version: .installed[].version, used_options: .installed[].used_options})' \
+  | jq 'map(select(.used_options | length > 0))' \
+  | jq 'group_by(.name)' \
+  | jq 'map({key: .[0].name, value: . | map({version: .version, used_options: .used_options})})' \
+  | jq 'from_entries'
