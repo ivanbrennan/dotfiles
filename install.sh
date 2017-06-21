cfd17bc682f3a6b8d8afd6393982a13a0693aa40
ivanbrennan <ivan.brennan@gmail.com>
2017-06-20 23:18:22 -0400

    refactor string handling
    Use printf where it makes sense to do so.

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ set -eu
 
 repo_root=$(cd "$(dirname "${BASH_SOURCE[0]}")" && git rev-parse --show-toplevel)
 
-col_green="\x1b[32;01m"
-col_reset="\x1b[39;49;00m"
+col_green='\033[32;01m'
+col_reset='\033[39;49;00m'
 
-echo -e "${col_green}::: Symlinking shell config files :::${col_reset}"
+printf ':::%b Symlinking shell config files %b:::\n' $col_green $col_reset
 bash "$repo_root/install/shell-symlinks.sh"
