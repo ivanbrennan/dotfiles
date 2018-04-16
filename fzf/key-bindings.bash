f8a9f56c7ec2055e608988aca9cbc4fb24583ac4
ivanbrennan <ivan.brennan@gmail.com>
2018-04-15 20:38:58 -0400

    fix fzf key-bindings

--- fzf/key-bindings.bash
+++ fzf/key-bindings.bash
@@ -9,14 +9,19 @@ bind '"\et": transpose-words'
 
 # Required to refresh the prompt after fzf
 bind '"\eR": redraw-current-line'
+bind '"\e^": history-expand-line'
 
+# Alt-Space - Paste the selected file path into the command line
 if [ $BASH_VERSINFO -gt 3 ]; then
     bind -x '"\e ": "fzf-file-widget"'
 elif __fzf_use_tmux__; then
     bind '"\e ": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
 else
-    bind '"\e ": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
+    bind '"\e ": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\eR \C-h"'
 fi
 
-# M-r history
+# ALT-R - Paste the selected command from history into the command line
 bind '"\er": " \C-e\C-u`__fzf_history__`\e\C-e\e^\eR"'
+
+# ALT-C - cd into the selected directory
+bind '"\ec": " \C-e\C-u`__fzf_cd__`\e\C-e\eR\C-m"'
