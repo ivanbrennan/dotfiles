e352619567c51085232377dbb1e8fd4ab490f8ec
ivanbrennan <ivan.brennan@gmail.com>
2020-02-05 10:07:59 -0500

    Shift-Space and other extended modifiers
    Reintroduce terminal support for extended modfiers like Shift-Space,
    through escape sequences. This will allow me to move fzf behavior
    currently bound to Meta-Space onto Shift-Space instead, freeing up
    Meta-Space for window manager (i3) use (most likely j4 or rofi).
    
    These extended modifiers are _not_ supported in gnome terminal (at least
    I haven't found a way to support them), so I'm going to switch to
    alacritty and/or urxvt.

--- fzf/key-bindings.bash
+++ fzf/key-bindings.bash
@@ -13,11 +13,11 @@ bind '"\e^": history-expand-line'
 
 # Alt-Space - Paste the selected file path into the command line
 if [ $BASH_VERSINFO -gt 3 ]; then
-    bind -x '"\e ": "fzf-file-widget"'
+    bind -x '"\e[32;2u": "fzf-file-widget"'
 elif __fzf_use_tmux__; then
-    bind '"\e ": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
+    bind '"\e[32;2u": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
 else
-    bind '"\e ": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\eR \C-h"'
+    bind '"\e[32;2u": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\eR \C-h"'
 fi
 
 # ALT-R - Paste the selected command from history into the command line
