4e272c74c7237d3a15fa719c48ef44419a2be1fa
ivanbrennan <ivan.brennan@gmail.com>
2020-08-22 23:37:35 -0400

    adjust key bindings

--- fzf/key-bindings.bash
+++ fzf/key-bindings.bash
@@ -11,13 +11,13 @@ bind '"\et": transpose-words'
 bind '"\eR": redraw-current-line'
 bind '"\e^": history-expand-line'
 
-# Shift-Space - Paste the selected file path into the command line
+# ALT-F - Paste the selected file path into the command line
 if [ $BASH_VERSINFO -gt 3 ]; then
-    bind -x '"\e[32;2u": "fzf-file-widget"'
+    bind -x '"\ef": "fzf-file-widget"'
 elif __fzf_use_tmux__; then
-    bind '"\e[32;2u": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
+    bind '"\ef": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
 else
-    bind '"\e[32;2u": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\eR \C-h"'
+    bind '"\ef": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\eR \C-h"'
 fi
 
 # ALT-R - Paste the selected command from history into the command line
